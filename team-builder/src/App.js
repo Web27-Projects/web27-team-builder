6206fd39f218a025954586ce66cb93db098633d5
Miley Wright <dishaliekwright@yahoo.com>
2020-01-29 14:16:27 -0500

    files structured

---
+++ team-builder/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from './Components/Form';
+import './App.css';
+import TeamMembers from './Components/TeamMembers';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div classNAme = 'container'>
+      Hello
+      <Form />
+      <TeamMembers />
+      </div>
+    </div>
+  );
+}
+
+export default App;
