6bab7cc648b544dd787471a5a09e197cb2ac489c
Miley Wright <dishaliekwright@yahoo.com>
2020-01-29 14:26:30 -0500

    set up my state for teamMembers

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from './Components/Form';
 import './App.css';
 import TeamMembers from './Components/TeamMembers';
 
 
 function App() {
+  const [teamMember, newTeamMember] = useState([
+    {
+      id: 1,
+      first_name: 'Miley' ,
+      last_name: 'Wright' ,
+      email: 'mileywright@yahoo.com',
+      github: 'MileyWright'
+    }, 
+    {
+      id: 2,
+      first_name: 'Brayden' ,
+      last_name: 'Chen' ,
+      email: 'braydenchen@yahoo.com',
+      github: 'BraydenChen'
+    }
+  ]);
+
+ 
+
   return (
     <div className="App">
       <div classNAme = 'container'>
